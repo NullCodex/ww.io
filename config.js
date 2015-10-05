57551ae53bdeaf9ec4a65e371ddf4832b1bb7ec8
NullCodex <jamesonyu95@gmail.com>
2015-10-04 21:18:50 -0400

    Testing

--- config.js
+++ config.js
@@ -7,6 +7,9 @@ module.export = function(app, io, db) {
     // Set .html as the default template extension
     app.set('view engine', 'html');
 
+    // Initialize the ejs template engine
+    app.engine('html', require('ejs').renderFile);
+
     // Tell express where it can find the templates
     app.set('views', __dirname + '/views');
 
