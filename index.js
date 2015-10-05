7c245c3013d4cb37b5f4d602dfbc9c19a784ace3
NullCodex <jamesonyu95@gmail.com>
2015-10-04 21:38:56 -0400

    Last one?

--- index.js
+++ index.js
@@ -1,47 +1,72 @@
+var app = require('express')();
+var mongodb = require('mongodb');
+var port = process.env.PORT || 8080;
+var uri = process.env.MONGOLAB_URI;
+
+mongodb.connect(uri, {server: {auto_reconnect: true}}, function(err, db) {
+  if(err) {
+    alert("Database could not load!");
+  } else {
+    var io = require('socket.io').listen(app.listen(port));
+    require('./config')(app, io, db);
+    require('./routes')(app, io, db);
+  }
+
+});
+
+
+
+/*
 var app = require ('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var express = require('express');
 var request = require('request');
 var path = require('path');
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
+var mongodb = require('mongodb');
+var uri = process.env.MONGOLAB_URI;
 
 var MAX_PLAYERS = 4;
 var numOfPlayers = 0;
 
-/*
-var mysql      = require('mysql');
- var connection = mysql.createConnection({
-   host     : 'localhost',
-   user     : 'root',
-   password : '',
-   port     : '3307',
-   database : 'riskdb',
-   socket   : 'tmp/mysql.sock' // don't know where the rest of the path is
- });
- connection.connect();
-/*
- connection.query('SELECT * from employees', function(err, rows, fields) {
-   if (!err)
-     console.log('The solution is: ', rows);
-   else
-     console.log('Error while performing Query.');
-     console.log(err);
- });
- connection.end();
-*/
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/main', function(req, res){
 
-    res.sendFile(path.join(__dirname, 'views', 'main.html'));
+  res.sendFile(path.join(__dirname, 'views', 'main.html'));
 });
 
 app.get('/game', function(req, res){
 
-    res.sendFile(path.join(__dirname, 'views', 'main.html'));
+  res.sendFile(path.join(__dirname, 'views', 'main.html'));
 });
+
+
+mongodb.connect(uri, {server: {auto_reconnect: true }}, function (err, db) {
+if(err)
+{
+  console.log("Database failed.");
+}
+else
+{
+
+      db.collection("messages").insert(
+      {
+        chatId: id
+      }
+    )
+
+      var io = require('socket.io').listen(app.listen(port));
+
+      // Require the configuration and the routes files, and pass
+      // the app and io as arguments to the returned functions.
+
+      require('./config')(app, io, db);
+      require('./routes')(app, io, db);
+      console.log('The application is running on http://localhost:' + port);
+}
 
 function initialize() {
     server.listen(port);
@@ -70,3 +95,5 @@ io.on('connection', function(socket) {
     });
 
 });
+
+*/
