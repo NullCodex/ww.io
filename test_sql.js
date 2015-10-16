123e22312d45cc91fcbad4f0d4c4a8e923758226
NullCodex <jamesonyu95@gmail.com>
2015-10-15 21:14:55 -0400

    Verified db function is good

--- test_sql.js
+++ test_sql.js
@@ -4,9 +4,9 @@ var uri = 'mongodb://localhost:27017/local';
 
 var updateName = function(db, callback) {
     db.collection('war').updateOne(
-        {"playerName" : "1"},
+        {"playerName" : "player1"},
         {
-            $set: {"playerName": 2}
+            $set: {"playerName": "Jameson"}
         }, function(err, results) {
             console.log(results);
             callback();
@@ -14,26 +14,40 @@ var updateName = function(db, callback) {
 };
 
 var changeTerritory = function(db, player1, player2, territoryName, callback) {
+    var territoryJSON = {};
+    territoryJSON["countryName"] = territoryName;
+    territoryJSON["soldiers"] = 0;
+    territoryJSON["tanks"] = 0;
+
+    var removeJSON = {};
+    removeJSON["countryName"] = territoryName;
+
     db.collection('war').updateOne (
         {"playerName": player1},
         {
-            $push: {"territories" : territoryName}
+            $push: {"territories" : territoryJSON}
+        }, function(err, result) {
+            db.collection('war').updateOne (
+                {"playerName": player2},
+                {
+                    $pull: {'territories' : removeJSON}
+                }, function (err, results) {
+                    callback();
+                });
         });
 
-    db.collection('war').updateOne (
-        {"playerName": player2},
-        {
-            $pull: {"territories.countryName":territoryName}
-        });
+
 }
 
-var updateArmy = function (db, playerName, territoryName, number, callback) {
-    db.collection('war').updateOne(
-        {"playerName": playerName , "territories.countryName": territoryName},
+var updateArmy = function (db, searchJSON, updateJSON, callback) {
+    console.log("asdasda");
+    db.collection('war').updateOne (
+        searchJSON,
         {
-            $inc: {"territories.soldier": number}
-        }
-    );
+            $inc: updateJSON
+        }, function(err, results) {
+            callback();
+        });
 }
 
 
@@ -42,9 +56,33 @@ mongodb.connect(uri, {server: {auto_reconnect: true}}, function(err, db) {
   if(err) {
     console.log("Database could not load!");
   } else {
-      updateName(db, function() {
-      db.close();
-  });
+    /*
+        var searchJSON = {};
+        searchJSON["territories.countryName"] = "China";
+        var updateJSON = {};
+        updateJSON["territories.$.soldiers"] = 2;
+        console.log(searchJSON);
+        console.log(updateJSON);
+        db.close();
+    */
+    /*
+        var searchJSON = {};
+        searchJSON["territories.countryName"] = "China";
+        var updateJSON = {};
+        updateJSON["territories.$.soldiers"] = 2;
+
+        updateArmy(db, searchJSON, updateJSON, function() {
+        db.close();
+    });
+    */
+    /*
+    updateName(db, function() {
+        db.close();
+    });
+*/
+    changeTerritory(db, "player2", "player1", "China", function () {
+        db.close();
+    })
   }
 
 });
