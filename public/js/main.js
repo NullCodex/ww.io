0a0e5951b109e7a05696bbfbb91263ea62a4719c
Kelvin Jiang <yu1256365@hotmail.com>
2015-10-27 23:37:18 -0400

    commit messages don't matter do they

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-	function showMap() {
-		window.location = 'http://calm-earth-7787.herokuapp.com/main.html'
-	}
+function showMap() {
+	window.location = 'http://calm-earth-7787.herokuapp.com/main.html'
+}
 
+$(document).ready(function(){
 	document.getElementById("map_button").addEventListener(
 		"click", showMap()
 	);
