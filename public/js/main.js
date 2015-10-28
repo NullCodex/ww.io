4d59bf63d13035db387163476c3ecb4f7d7166ff
Kelvin Jiang <yu1256365@hotmail.com>
2015-10-27 23:34:25 -0400

    TODO: squash small commits together

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
-function showMap() {
-	window.location = 'http://calm-earth-7787.herokuapp.com/main.html'
-}
+$(document).ready(function(){
+	function showMap() {
+		window.location = 'http://calm-earth-7787.herokuapp.com/main.html'
+	}
 
-document.getElementById("map_button").addEventListener(
-	"click", showMap()
-);
+	document.getElementById("map_button").addEventListener(
+		"click", showMap()
+	);
+}
